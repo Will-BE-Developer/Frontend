0dbc30a89c4f5cb991dc678a708d856ec622f2ba
llama-ste <andong1212@gmail.com>
2022-05-09 10:26:39 +0900

    Feat: kakao redirect uri 수정 & api추가

--- src/pages/KakaoRedirect.jsx
+++ src/pages/KakaoRedirect.jsx
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import instance from "../apis/axios";
+
 const KakaoRedirect = () => {
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get("code");
+
+  useEffect(() => {
+    instance
+      .post(`${process.env.REACT_APP_API_JURI_URL}/user/kakao/callbak`, {
+        grantCode: code,
+      })
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [code, navigate]);
+
   return <>kakao rediext</>;
 };
 
