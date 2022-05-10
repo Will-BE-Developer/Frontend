9a012b4eb0329b1912de252aec8830eddb15d704
algoroot <algoroot524@gmail.com>
2022-05-10 19:02:02 +0900

    Style : icon 추가

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -13,8 +13,13 @@ const AppLayout = ({ children }) => {
 };
 
 const MainContainer = styled.main`
+  /* width: ${(props) => (props._width ? props._width : "max-content")}; */
   max-width: 1200px;
   margin: 0 auto;
+
+  ${({ theme }) => theme.device.mobile} {
+    padding: 0px 1rem;
+  }
 `;
 
 export default AppLayout;
