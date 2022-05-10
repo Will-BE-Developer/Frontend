d4d357128e6f51caa08377705862c30d3adcc9f6
algoroot <algoroot524@gmail.com>
2022-05-10 19:06:47 +0900

    Feat : FeedbackDetail 수정 삭제 테스트

---
+++ src/pages/FeedBack/FeedBack.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
+import GlobalCard from "../../components/UI/GlobalCard";
+
+import Dropdown from "../../components/UI/GlobalDropDown";
+import { getFeedback as getFeedbackApi } from "../../apis/async.js";
+
+const FeedBack = () => {
+  const [data, setData] = useState([]);
+
+  const [selectedDate, setSelectedDate] = useState("(정렬)");
+  const [selectedCategory, setSelectedCategory] = useState("전체보기");
+
+  useEffect(() => {
+    getFeedbackApi().then((data) => {
+      setData(data.interviews);
+    });
+  }, []);
+
+  const dateList = ["최신순", "오래된순"];
+  const categoryList = ["Frontend", "Backend"];
+
+  return (
+    <Container>
+      <div className="dropDown_container">
+        <Dropdown
+          selected={selectedDate}
+          setSelected={setSelectedDate}
+          options={dateList}
+        />
+        <Dropdown
+          selected={selectedCategory}
+          setSelected={setSelectedCategory}
+          options={categoryList}
+        />
+      </div>
+      <div className="card_wrap">
+        {data?.map((card) => {
+          return <GlobalCard key={card.id} card={card} />;
+        })}
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  padding: 0;
+  margin-top: 100px;
+  & .dropDown_container {
+    display: flex;
+    justify-content: right;
+    left: 0;
+    width: 316px;
+    margin-left: auto;
+  }
+
+  & .card_wrap {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    animation: fadeInBottom 1s;
+    transform: translateY(0%);
+
+    @keyframes fadeInBottom {
+      from {
+        opacity: 0;
+        transform: translateY(30%);
+      }
+      to {
+        opacity: 1;
+      }
+    }
+    ${({ theme }) => theme.device.tablet} {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    ${({ theme }) => theme.device.mobile} {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+`;
+
+export default FeedBack;
---
+++ src/pages/FeedBack/FeedbackUpdate.jsx
@@ -0,0 +1,173 @@
+import React, { useRef } from "react";
+import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import theme from "../../styles/theme";
+import styled from "styled-components";
+import GlobalButton from "../../components/UI/GlobalButton";
+import { GrRefresh } from "react-icons/gr";
+import { updateFeedbackDetail as updateDetailApi } from "../../apis/async.js";
+
+const FeedbackUpdate = () => {
+  const navigate = useNavigate();
+  const content = useRef();
+  const { state } = useLocation();
+
+  const { video, data } = state;
+  console.log(data);
+  const [noteInfo, setNoteInfo] = useState({
+    noteText: data.note,
+    noteCount: 0,
+  });
+  const [isPublic, setIsPublic] = useState(data.isPublic);
+  const clickUpdateHandler = () => {
+    const cardId = data.id;
+    const updateData = {
+      note: noteInfo.noteText,
+      isPublic,
+    };
+
+    updateDetailApi(cardId, updateData).then((data) => {
+      console.log(data);
+      if (isPublic === true) {
+        navigate(`/feedback/${cardId}`, { replace: true });
+      } else {
+        navigate("/mypage/history", { replace: true });
+      }
+    });
+  };
+
+  const clickCancleHandler = () => {
+    if (isPublic === true) {
+      navigate(`/feedback/detail/${data.id}`, { replace: true });
+    } else {
+      navigate("/mypage/history", { replace: true });
+    }
+  };
+
+  return (
+    <FormLayout>
+      <div className="video_layout">
+        <video controls src={video}></video>
+      </div>
+      <p>공개 여부</p>
+      <div className="radioGroup">
+        <label>
+          <input
+            onChange={() => setIsPublic(false)}
+            type="radio"
+            name="isPublic"
+            checked={isPublic === false && "checked"}
+          />
+          나만 보기
+        </label>
+        <label>
+          <input
+            onChange={() => setIsPublic(true)}
+            type="radio"
+            name="isPublic"
+            checked={isPublic === true && "checked"}
+          />
+          전체 공개
+          {isPublic && (
+            <span className="noti">*피드백 게시판에 게시됩니다.</span>
+          )}
+        </label>
+      </div>
+      <div>
+        <div>
+          <span>{data.question.category}</span>
+          <h2>{data.question.contents}</h2>
+        </div>
+        <p>내용</p>
+        <textarea
+          onChange={(e) =>
+            setNoteInfo({
+              noteText: e.target.value,
+              noteCount: e.target.value.length,
+            })
+          }
+          placeholder="내용을 작성해주세요"
+          maxLength={500}
+          rows={5}
+          value={noteInfo.noteText}
+        ></textarea>
+        <p
+          style={{ textAlign: "end", color: "#666666" }}
+        >{`${noteInfo.noteCount} / 500`}</p>
+      </div>
+      <div className="btnWrapper">
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <GlobalButton
+            onClick={clickCancleHandler}
+            text="취소"
+            margin="0px 10px 0px 0px"
+            background={theme.colors.white}
+            color={theme.colors.black}
+            border="1px solid rgba(130, 130, 130, 0.2)"
+          />
+          <GlobalButton onClick={clickUpdateHandler} text="수정" />
+        </div>
+      </div>
+    </FormLayout>
+  );
+};
+
+const FormLayout = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  text-align: start;
+  video {
+    max-width: 750px;
+    width: 100%;
+  }
+
+  & .video_layout {
+    display: flex;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.headerBgColor};
+    border-radius: 6px;
+    margin-bottom: 20px;
+  }
+
+  & p {
+    margin-top: 8px;
+    font-size: ${({ theme }) => theme.fontSize["12"]};
+  }
+
+  & textarea {
+    margin-top: 8px;
+    padding: 11px 16px;
+    border: 1px solid rgba(130, 130, 130, 0.2);
+    border-radius: 4px;
+    width: 100%;
+  }
+
+  & .radioGroup {
+    display: flex;
+    gap: 10px;
+    margin: 8px 0px 20px 0px;
+    font-size: ${({ theme }) => theme.fontSize["14"]};
+  }
+
+  & .radioGroup input {
+    margin: 0px 5px 0px 0px;
+  }
+
+  & .radioGroup label {
+    display: flex;
+    align-items: center;
+  }
+
+  & .noti {
+    font-size: ${({ theme }) => theme.fontSize["12"]};
+    color: #666666;
+    margin-left: 5px;
+  }
+
+  & .btnWrapper {
+    display: flex;
+    justify-content: space-between;
+    margin: 20px 0px;
+  }
+`;
+export default FeedbackUpdate;
