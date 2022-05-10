58c20ca329a73a21f1c446ac8c876c1f39b12d3d
algoroot <algoroot524@gmail.com>
2022-05-10 19:00:04 +0900

    Feat : FeedbackDetail put, delete  & Mypage get & scrap get, post, put API 추가

--- src/apis/async.js
+++ src/apis/async.js
@@ -1,12 +1,10 @@
 import instance from "./axios.js";
+import { getCookie } from "../shared/cookies";
+const token = getCookie("token");
 
 export const getFeedback = async () => {
   try {
-    const res = await instance.get("/api/interviews/", {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-    });
+    const res = await instance.get("/api/interviews/");
     return res.data;
   } catch (err) {
     return err;
@@ -14,10 +12,12 @@ export const getFeedback = async () => {
 };
 
 export const getFeedbackDetail = async (cardId) => {
-  console.log(cardId);
   try {
-    const res = await instance.get(`/api/interviews/${cardId}`);
-    console.log(res);
+    const res = await instance.get(`/api/interviews/${cardId}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
     return res.data;
   } catch (err) {
     console.log(err.response);
@@ -27,7 +27,11 @@ export const getFeedbackDetail = async (cardId) => {
 
 export const getFeedbackDetailVideo = async (cardId) => {
   try {
-    const res = await instance.get(`/api/interviews/${cardId}`);
+    const res = await instance.get(`/api/interviews/${cardId}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
     const convertVideo = await instance.get(res.data.interview.video, {
       responseType: "blob",
     });
@@ -37,11 +41,18 @@ export const getFeedbackDetailVideo = async (cardId) => {
     return err;
   }
 };
-
-export const editFeedbackDetail = async (cardId) => {
-  console.log(cardId);
+export const updateFeedbackDetail = async (cardId, updateData) => {
+  console.log(cardId, updateData);
   try {
-    const res = await instance.get(`/api/interviews/${cardId}`);
+    const res = await instance.put(
+      `/api/interviews/${cardId}`,
+      { updateData },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
     console.log(res);
     return res.data;
   } catch (err) {
@@ -53,7 +64,11 @@ export const editFeedbackDetail = async (cardId) => {
 export const deleteFeedbackDetail = async (cardId) => {
   console.log(cardId);
   try {
-    const res = await instance.delete(`/api/interviews/${cardId}`);
+    const res = await instance.delete(`/api/interviews/${cardId}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
     console.log(res);
     return res.data;
   } catch (err) {
@@ -64,9 +79,7 @@ export const deleteFeedbackDetail = async (cardId) => {
 
 export const signupEmailCheck = async (email) => {
   try {
-    const res = await instance.get(
-      `${process.env.REACT_APP_API_JURI_URL}/signup/${email}`
-    );
+    const res = await instance.get(`/signup/${email}`);
     if (res.status === 200) {
       alert(res.data.msg);
       return res.data;
@@ -81,21 +94,61 @@ export const signupEmailCheck = async (email) => {
   }
 };
 
-// export const getUserCard = async (email) => {
-//   try {
-//     const res = await instance.get(
-//       `${process.env.REACT_APP_API_JURI_URL}/signup/${email}`
-//     );
-//     if (res.status === 200) {
-//       alert(res.data.msg);
-//       return res.data;
-//     }
-//   } catch (err) {
-//     if (err.response.status === 409) {
-//       alert(err.response.data.msg);
-//       return err.response.data;
-//     }
-//     console.log("이메일 중복 체크 오류: ", err.response);
-//     return err;
-//   }
-// };
+//MyPage
+export const getUserCard = async () => {
+  try {
+    const res = await instance.get(`/api/users/me/interviews`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return res.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export const getUserScrap = async () => {
+  try {
+    const res = await instance.get(`/api/users/me/scraps`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return res.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export const addScrap = async (id) => {
+  try {
+    const res = await instance.post(
+      `/api/scraps/${id}`,
+      {},
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export const undoScrap = async (id) => {
+  try {
+    const res = await instance.delete(`/api/scraps/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    return err.response;
+  }
+};
